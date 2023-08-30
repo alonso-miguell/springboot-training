46e11cd698dc9796fed2a8fe774da63387d7fddc
miguel alonso <migalonso@deloitte.com>
2023-08-30 13:57:55 -0600

    internationalization

--- src/main/java/com/deloitte/spring/demo/controllers/HelloWorldController.java
+++ src/main/java/com/deloitte/spring/demo/controllers/HelloWorldController.java
@@ -2,11 +2,20 @@ package com.deloitte.spring.demo.controllers;
 
 
 import com.deloitte.spring.demo.pojos.UserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private ResourceBundleMessageSource messageSource;
 
     @GetMapping("/helloworld1")
     public String helloWorld() {
@@ -16,6 +25,18 @@ public class HelloWorldController {
     @GetMapping("/helloworld-bean")
     public UserDetails helloWorldBean() {
         return new UserDetails("Miguel", "Alonso", "CDMX");
+    }
+
+    @GetMapping("/hello-int")
+    public String getMessagesInI18NFormat(@RequestHeader(name = "Accept-Language", required=false) String locale) {
+        return messageSource.getMessage("label.hello", null, new Locale(locale));
+
+    }
+
+    @GetMapping("/hello-int2")
+    public String getMessagesInI18NFormat2() {
+        return messageSource.getMessage("label.hello", null, LocaleContextHolder.getLocale());
+
     }
 
 }
